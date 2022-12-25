d7ad47e668af88d84920f0890a3a86efcef162f3
Amit Singh Rana <ranaa73000@gmail.com>
2022-12-25 18:32:45 +0530

    Adding check for valid username

--- controllers/user.js
+++ controllers/user.js
@@ -21,6 +21,23 @@ const getAllUsers = async (req, res) => {
 // Adds a new user to DB
 const addUser = async (req, res) => {
   try {
+    // Validate if the username is correct
+    let matchedUsername = newUser.username.match(
+      /(_*[A-Za-z0-9]+)(_*[A-Za-z0-9]*)*/
+    );
+
+    if (!matchedUsername || matchedUsername[0] != newUser.username) {
+      console.log("Not Matching");
+      return res.status(500).json({
+        ok: false,
+        error: {
+          message: `The username passed is not valid, below are rules - \n
+        \t 1. Username must contain only [a-z] [A-Z] [0-9] and _ characters.
+        \t 2. Username must contain at least one character instead of _.`,
+        },
+      });
+    }
+
     // Create the new user using the User model
     const newUser = new User(req.body);
 
